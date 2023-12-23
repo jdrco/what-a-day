b552f74cd9bd558406d9584e109a7102a3425812
Jared Drueco <drueco03@gmail.com>
2023-12-23 12:44:14 -0700

    Initial Commit

---
+++ whataday/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  whataday
+//
+//  Created by Jared Drueco on 2023-12-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ whataday/whatadayApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatadayApp.swift
+//  whataday
+//
+//  Created by Jared Drueco on 2023-12-23.
+//
+
+import SwiftUI
+
+@main
+struct whatadayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
